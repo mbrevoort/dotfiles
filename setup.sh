23396f0605770da53067705ae636af2679a114cc
Milan Pavlik <pavlik.mil@gmail.com>
2022-02-21 11:33:50 +0000

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Setting up dotfiles"
+
+
+if [[ -z "$ZSH_VERSION" ]]; then
+    echo "Installing zsh"
+    sudo apt install zsh
+fi
+
+if [[ ! -d "~/.oh-my-zsh" ]]; then
+    echo "Installing Oh My Zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
