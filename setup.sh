bf9f6fe1a5dda058ca128885cfa61d698d92b634
Mike Brevoort <mike.brevoort@gitpod.io>
2024-05-07 22:16:59 +0000

    tweak setup for devcontainers

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ set -ex
 
 echo "Setting up dotfiles"
 
+SCRIPT_DIR=$(realpath $(dirname "$0"))
+cd $SCRIPT_DIR
 
 if [[ -z "$ZSH_VERSION" ]]; then
     echo "Installing zsh"
@@ -15,11 +17,6 @@ if [[ ! -d "~/.oh-my-zsh" ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
-if [[ -d "/home/gitpod/.dotfiles" ]]; then
-    # In gitpod, dotfiles are stored in this directory
-    pushd "/home/gitpod/.dotfiles"
-        mv .zshrc ~/.zshrc
-    popd
-fi
+mv .zshrc ~/.zshrc
 
 export SHELL=zsh
